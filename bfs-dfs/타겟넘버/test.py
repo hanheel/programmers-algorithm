b8cb568d78d79adf8b9b0902c82ebdf22a24f71b
hanheel <hanhee630630@gmail.com>
2025-11-04 23:06:08 +0900

    add: 타겟넘버 풀이 추가

--- bfs-dfs/타겟넘버/test.py
+++ bfs-dfs/타겟넘버/test.py
@@ -1,19 +1,22 @@
 def solution(numbers, target):
     # index, sum의 형식
     stack=[]
-    stack.append([0,0])
+    stack.append((0,0))
     answer=0
     while stack:
 
         a,b = stack.pop()
+        # 주어진 조합을 모두 연산했을 때 target과 값이 같은 경우
         if a==len(numbers) and b==target:
             answer+=1
             continue
+        # 주어진 조합을 모두 연산했지만 target과 같지 않은 경우
         if a==len(numbers) and b!=target:
             continue
             
-        stack.append([a+1, b+numbers[a]])
-        stack.append([a+1, b-numbers[a]])
+        # 스택에 다음 조합 push
+        stack.append((a+1, b+numbers[a]))
+        stack.append((a+1, b-numbers[a]))
     return answer
             
             
